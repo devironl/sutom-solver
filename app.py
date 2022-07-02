7637eec1f733c8daf860697cdc1fc20410ae49c1
Louis de Viron <louis@datatext.eu>
2022-07-02 08:38:46 +0200

    Add grey letters

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import re
 from unidecode import unidecode
 from flask import Flask, render_template, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -14,10 +15,16 @@ def all_yellow_in_word(yellow_letters, word):
      
     return True
 
-def lookup(attempt, yellow_letters):
+def grey_in_word(grey_letters, word):
+    for letter in grey_letters:
+        if unidecode(letter.upper()) in word:
+            return True
+    return False
+
+def lookup(attempt, yellow_letters, grey_letters):
     regex = re.compile(f"^{attempt.upper().strip()}$")
     for word in dico:
-        if regex.search(word) and all_yellow_in_word(yellow_letters, word):
+        if regex.search(word) and all_yellow_in_word(yellow_letters, word) and not grey_in_word(grey_letters, word):
             yield word
 
 @app.route('/', methods=['GET', 'POST'])
@@ -26,8 +33,13 @@ def index():
     if request.method == 'POST' and "attempt" in request.form:
         attempt = request.form["attempt"]
         yellow_letters = request.form["yellow"]
-        candidates = list(lookup(attempt, yellow_letters))
-        return render_template('index.html', candidates=candidates)
+        grey_letters = request.form["grey"]
+        candidates = list(lookup(attempt, yellow_letters, grey_letters))
+        print(candidates)
+        if len(candidates) == 0:
+            return render_template("index.html", error_message="Error")
+        else:
+            return render_template('index.html', candidates=candidates)
     return render_template('index.html')
 
 
@@ -36,7 +48,7 @@ def index():
 
     
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5002)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get("PORT", 5002))
     # while True:
     #     attempt = input("attempt:\n> ")
     #     yellow = input("yellow:\n> ")
